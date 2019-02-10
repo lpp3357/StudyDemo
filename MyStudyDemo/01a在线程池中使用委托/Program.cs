169726ef7bfc52d5660bf8962aee9eb0c57dd5d6
lpp3357 <18398463357@163.com>
2019-02-10 17:22:19 +0800

    新增线程池中调用委托与线程池中放入异步操作

---
+++ MyStudyDemo/01a在线程池中使用委托/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace _01a在线程池中使用委托
+{
+    /// <summary>
+    /// 该例重点是使用 BeginInvoke 来使用委托 ，使用 BeginInvoke之后必须使用EndInvoke
+    /// </summary>
+    class Program
+    {
+        //定义委托
+        private delegate string RunOneThreadPool(out int threadId);
+        static void Main(string[] args)
+        {
+            int threadID = 0;
+            //创建线程
+            var t = new Thread(() => Test(out threadID));
+            t.Start();
+            t.Join();
+            WriteLine($"线程Id：{threadID}");
+            //实例化委托
+            RunOneThreadPool poolDelegate = Test;
+            //执行委托
+            IAsyncResult r = poolDelegate.BeginInvoke(out threadID, Callback, "一个异步回调---001");
+            r.AsyncWaitHandle.WaitOne();
+            string result = poolDelegate.EndInvoke(out threadID, r);
+            WriteLine($"线程池工作线程Id：{threadID}");
+            WriteLine(result);
+            ReadKey();
+        }
+
+        /// <summary>
+        /// 定义回调方法
+        /// </summary>
+        /// <param name="ar"></param>
+        static void Callback(IAsyncResult ar)
+        {
+            WriteLine("开始执行回调");
+            WriteLine($"回调状态：{ar.AsyncState}");
+            WriteLine($"是否线程池线程：{CurrentThread.IsThreadPoolThread}");
+            WriteLine($"工作线程池ID:{CurrentThread.ManagedThreadId}");
+        }
+
+        static string Test(out int threadId)
+        {
+            WriteLine("开始。。。");
+            WriteLine($"是否线程池线程：{CurrentThread.IsThreadPoolThread}");
+            //线程休眠
+            Sleep(TimeSpan.FromSeconds(2));
+            threadId = CurrentThread.ManagedThreadId;
+            return $"工作线程池ID是：{CurrentThread.ManagedThreadId}";
+        }
+
+    }
+}
---
+++ MyStudyDemo/01b向线程池中放入异步操作/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace _01b向线程池中放入异步操作
+{
+    /// <summary>
+    /// 该例重点是使用线程池来执行方法，即为 向线程池中放入异步操作，此时方法为异步执行
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //定义常量
+            const int x = 1;
+            const int y = 2;
+            const string lambdaState = "lambda state 2";
+            ThreadPool.QueueUserWorkItem(AsyncOperation);
+            Sleep(TimeSpan.FromSeconds(1));
+
+            ThreadPool.QueueUserWorkItem(AsyncOperation, "异步状态");
+            Sleep(TimeSpan.FromSeconds(1));
+
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                WriteLine($"操作状态：{state}");
+                WriteLine($"工作线程Id：{CurrentThread.ManagedThreadId}");
+                Sleep(TimeSpan.FromSeconds(2));
+            }, "lambde state");
+
+            //使用闭包机制
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                WriteLine($"操作状态：{x + y},{lambdaState}");
+                WriteLine($"工作线程Id：{CurrentThread.ManagedThreadId}");
+                Sleep(TimeSpan.FromSeconds(2));
+            }, "lambde state");
+            Sleep(TimeSpan.FromSeconds(2));
+            ReadLine();
+        }
+
+        static void AsyncOperation(object state)
+        {
+            WriteLine($"操作状态：{state ?? "(NULL)"}");
+            WriteLine($"工作线程Id：{CurrentThread.ManagedThreadId}");
+            //线程休眠
+            Sleep(TimeSpan.FromSeconds(2));
+        }
+
+
+    }
+}
